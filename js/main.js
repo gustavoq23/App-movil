a12978e18ce2d86fd88d21f178940afc45b80189
Arthur <arturojose21@hotmail.com>
2022-03-04 10:21:47 -0500

    js and css splash update

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+const splash = document.querySelector('.splash');
+document.addEventListener('DOMContentLoaded', (e)=>{
+  setTimeout(()=>{
+    splash.classList.add('display-none');
+  },2000);
+})

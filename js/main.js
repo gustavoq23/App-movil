98d2faca8ff9e7e1ec651342247232c3ab2a1c68
AndreasVeiltl <andreas.veiltl@st.oth-regensburg.de>
2022-03-04 10:19:27 -0500

    added function saveData()

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+function saveData() {
+  var array = ['nombre', 'apellido', 'numero', 'direcion', 'ocupacion'];
+  array.forEach(element => {
+    var value = document.getElementById(element).value;
+    window.localStorage.setItem(element, value);
+  });
+}
+
+function readData() {
+  //Read Data from localStorage
+
+}
